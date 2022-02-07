d673c66dfdb459d3a541ef53994ea3b20ba07067
ADARSH MENON <88380337+Adarsh-Menon@users.noreply.github.com>
2022-02-07 07:24:45 +0000

    done

--- p3original.c
+++ p3original.c
@@ -7,11 +7,35 @@ int input(){
   return n;
 }
 
-int comp(int n,int a[n]){
-  for(int i=0; i<=n: i++){
-    total=n/i
-    if  n/i==1{
-      
+int comp(int n){
+  float total;
+  int composite=0;
+  for(int i=1; i<=n; i++){
+    total=n/i;
+    if (total==0 || total>=1){
+      composite=composite+1;
     }
+    else{
+        return composite;
+        }
   }
+  printf("%d",composite);
+  }
+
+
+void output(int n,int composite){
+  if (composite>2){
+    printf("%d is composite number",n);
+  }
+  else{
+    printf("%d is not composite number",n);
+  }
+}
+
+int main(){
+  int n,m;
+  n=input();
+  m=comp(n);
+  output(n,m);
+  return 0;
 }
---
+++ p6original.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+void input(char a){
+  printf("enter char ");
+  scanf("%s",a);
+}
+
+int strrev(char a){
+  for(int=0;a[i]; i-- ){
+    
+  }
+
+}
