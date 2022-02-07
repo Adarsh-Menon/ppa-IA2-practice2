a2d975daddeac82b47cff571a234a6d18c1ea18b
ADARSH MENON <88380337+Adarsh-Menon@users.noreply.github.com>
2022-02-07 06:57:11 +0000

    DONE-07/02/22

---
+++ p3original.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int input(){
+  int n;
+  printf("enter the a composite number");
+  scanf("%d",&n);
+  return n;
+}
+
+int comp(int n,int a[n]){
+  for(int i=0; i<=n: i++){
+    total=n/i
+    if  n/i==1{
+      
+    }
+  }
+}
